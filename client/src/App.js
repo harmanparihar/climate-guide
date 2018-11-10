22ff0e9e80f6c8e41ea12da37636f442272adb83
Maggie <maggievu91@gmail.com>
2018-11-09 18:00:20 -0800

    re-organize folder structures for client/src and css/sass

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import Header from './Components/Header';
-import Main from './Components/Main';
-import Sidebar from './Components/Sidebar';
-import Footer from './Components/Footer';
+import Header from './components/Header';
+import Main from './components/discussion-board/Main';
+import Sidebar from './components/discussion-board/Sidebar';
+import Footer from './components/Footer';
 
 import './css/style.css';
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Header />
-      <Sidebar />
-      <Main />
-      <Footer />
+          <Header />
+          <Sidebar />
+          <Main />
+          <Footer />
       </div>
     );
   }
