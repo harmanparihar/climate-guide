cc00122f620a2c74a0523f52b6f89717b8bb60ae
Diego <malucorp@gmail.com>
2018-11-15 15:32:23 -0800

    Add css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import NavMenu from './navigation/NavMenu';
 import SupportButton from './navigation/SupportButton';
-import logoHor from './assets/logo/cg_hor.svg';
+import logoSymbol from './assets/logo/cg_symbol.svg';
 
 class Header extends Component {
     render() {
@@ -10,7 +10,7 @@ class Header extends Component {
                 <div className="site-title">
                     <a href="#home">
                         {/* <img className="site-logo" /> */}
-                        <span className="site-logo"><img src={logoHor} alt="ClimateGuide" className="hor_logo"/></span>
+                        <span className="site-logo"><img src={logoSymbol} alt="ClimateGuide" className="hor_logo"/></span>
                     </a>
                 </div>
 
