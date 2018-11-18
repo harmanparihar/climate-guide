a3d333b6532201780a5d62a59607ee171ff6ff8d
harmanparihar <harmanparihar18@gmail.com>
2018-11-18 15:16:14 -0800

    merged changes

--- client/src/components/discussion-board/Sidebar.js
+++ client/src/components/discussion-board/Sidebar.js
@@ -7,21 +7,14 @@ export default class Sidebar extends React.Component{
         <div className="sidebar">
         <div>
         <h4>Welcome Username!</h4>
-        <button>
-        Logout
-        </button>
         </div>
 
         <h4>Get Involved</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique corrupti repellat eum ipsum esse reiciendis, aut, iste tenetur necessitatibus natus consequatur cum ex doloribus nam ratione, sint quos aperiam.
          Aliquid!</p>
-         <h4>Quick Links</h4>
-         <Link to="/" className="btn btn-link text-secondary">
-         <span className="text-tertiary">Home Page</span>
-         </Link>
-         <Link to="/causes" className="btn btn-link text-secondary">
-         <span className="text-tertiary">Causes</span>
-         </Link>
+         <button>
+         Logout
+         </button>
         </div>
     );
   }
