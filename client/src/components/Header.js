f56f56d8954b0663294ea54ffaa726593ffc557e
Diego <malucorp@gmail.com>
2018-11-18 16:31:50 -0800

    Add animation fixed header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,28 +4,39 @@ import NavMenu from './navigation/NavMenu';
 import SupportButton from './navigation/SupportButton';
 import logoSymbol from './assets/logos/cg_symbol.svg';
 import Search from './navigation/Search'
-import MobileMenu from './navigation/MobileMenu'
 
 class Header extends Component {
-    render() {
-        return (
-            <header className="site-header">
-            <div className="site-title">
-            <Link to="/">
-        {/* <img className="site-logo" /> */}
-        <span className="site-logo"><img src={logoSymbol} alt="ClimateGuide" className="hor_logo"/></span>
-        </Link>
-        </div>
+ state = {
+    isTop: true,
+};
 
-        <NavMenu className="header-nav" />
+componentDidMount() {
+    document.addEventListener('scroll', () => {
+      const isTop = window.scrollY < 100;
+      if (isTop !== this.state.isTop) {
+          this.setState({ isTop })
+      }
+  })
 
-        <Search />
+}
+render() {
+    return (
+        <header className={this.state.isTop ? 'site-header' : 'site-header slide'}>
+        <div className="site-title">
+        <Link to="/">
+    {/* <img className="site-logo" /> */}
+    <span className="site-logo"><img src={logoSymbol} alt="ClimateGuide" className="hor_logo"/></span>
+    </Link>
+    </div>
 
-        <SupportButton />
-        <MobileMenu /> 
-        </header>
-        );
-    }
+    <NavMenu className="header-nav" />
+
+    <Search />
+
+    <SupportButton />
+    </header>
+    );
+}
 }
 
 export default Header;
