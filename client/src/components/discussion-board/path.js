eb370a4a4d54b7dbb8b16d1ea303223ca77bf07c
harmanparihar <harmanparihar18@gmail.com>
2018-12-04 23:50:45 -0800

    path_backend modifies

--- client/src/components/discussion-board/path.js
+++ client/src/components/discussion-board/path.js
@@ -1,3 +1,3 @@
-const path_backend = "http://35.235.105.67:8081"; //change to http://localhost:8080
+const path_backend = "http://35.235.105.67:80"; //change to http://localhost:8080
 
 export default path_backend;
